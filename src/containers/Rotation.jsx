5368488b203e4cbc2320cc63d4d576b7eb1da14b
Jonnathan <jonzxz12@gmail.com>
2024-04-02 17:11:31 -0600

    slider created implemented npm install react-slick and its styles slick-slider

--- src/containers/Rotation.jsx
+++ src/containers/Rotation.jsx
@@ -1,9 +1,40 @@
 import './Rotation.css';
+import yasuoLogo from '../img/yasuoLogo.jpeg'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick';
 
 function Rotation() {
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="Rotation">
-      
+      <h2>Free champion Rotation</h2>
+      <Slider {...settings} className='rotationSlider'>
+        <div>
+            <h3>Yasuo1</h3>
+            <img src={yasuoLogo} alt="Champion" />
+        </div>
+        <div>
+            <h3>Yasuo2</h3>
+            <img src={yasuoLogo} alt="Champion" />
+        </div>
+        <div>
+            <h3>Yasuo3</h3>
+            <img src={yasuoLogo} alt="Champion" />
+        </div>
+        <div>
+            <h3>Yasuo4</h3>
+            <img src={yasuoLogo} alt="Champion" />
+        </div>
+      </Slider>
     </div>
   );
 }
