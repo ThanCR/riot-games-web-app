ccf94ea7cdff52661bbb4ce3fee9ded63bccb5d4
Jonnathan <jonzxz12@gmail.com>
2024-04-03 02:30:50 -0600

    first implementations of tier list table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './containers/NavBar';
 import Rotation from './containers/Rotation';
+import TierList from './containers/TierList';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Rotation/>
+      <TierList/>
     </div>
   );
 }
